5d869b607374e35e3367c2504569571d08431e8a
ilvbowen <819603146@qq.com>
2017-06-12 01:07:44 +0800

    Initial commit

---
+++ src/main/java/com/nyist/java/TeamsystemApplication.java
@@ -0,0 +1,11 @@
+package com.nyist.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsystemApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TeamsystemApplication.class, args);
+	}
+}
